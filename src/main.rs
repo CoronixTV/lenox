57cffca7f5f00c3ef3654e90238f86c0bda01089
Len Blum <mail@lenblum.de>
2024-04-02 16:58:55 +0200

    added user file

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,14 @@ mod pi;
 use core::arch::asm;
 
 use gpio::OutPin;
-
+mod user;
  mod pwm;
 
 #[link_section = ".text._start"]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let pin = gpio::get_out_pin(16);
-    loop {
-        pin.set(gpio::State::High);
-        sleep::sleep(1.0, pi::Raspberrys::Pi3Plus);
-        pin.set(gpio::State::Low);
-        sleep::sleep(1.0, pi::Raspberrys::Pi3Plus);
-    }
+    user::main();
+    loop {}
 }
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
